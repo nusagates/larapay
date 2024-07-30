9b71e99b860064efbbeb7c7c3b1bce0dec7409fa
Irnovi <huiralb@gmail.com>
2024-07-30 21:33:03 +0700

    doc

--- src/Facades/Ipaymu.php
+++ src/Facades/Ipaymu.php
@@ -1,6 +1,12 @@
 <?php
 namespace Nusagates\Larapay\Facades;
 
+/**
+ * @method static Ipaymu getTransactionById(int $trx_id) ID Transaksi iPaymu
+ * @method static Ipaymu register(string $name, string $email, string $phone, string $password) Registrasi iPaymu
+ * @method static Ipaymu directPayment(Buyer $buyer, int $amount, Payment $payment, ?Product $product, ?string $account): mixed
+ */
+
 class Ipaymu extends \Illuminate\Support\Facades\Facade
 {
     /**
